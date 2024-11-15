03a6900078b5dcc18b2efb05dcab367277ad60c4
Augusto <afildefonso16@gmail.com>
2024-11-15 19:53:34 -0300

    Arrumando Merge-Sort

--- merge.c
+++ merge.c
@@ -2,19 +2,47 @@
 #include <stdlib.h>
 #include <time.h>
 
-void merge(int *vec, int ini, int fim) {
+void merge(int *vec, int ini, int fim, int meio) {
+    int i, j, k, n1, n2;
 
+    n1 = meio - ini + 1;
+    n2 = fim - meio;
+
+    int L[n1+1], R[n2+1];
+
+    for (i = 0; i < n1; i++) {
+        L[i] = vec[ini + i];
+    }
+
+    L[n1] = 9999;
+
+    for(j = 0; j < n2; j++) {
+        R[j] = vec[meio + 1 + j];
+    }
+
+    R[n2] = 9999;
+
+    i = j = 0;
+
+    for (k = ini; k <= fim; k++) {
+        if (L[i] <= R[j]) {
+            vec[k] = L[i];
+            i++;
+        } else {
+            vec[k] = R[j];
+            j++;
+        }
+    }
 }
 
 void merge_sort(int *vec, int ini, int fim) {
-    if(ini == fim) {
-        return;
+    if(ini < fim) {
+        int meio = (ini + fim) / 2;
+        merge_sort(vec, ini, meio);
+        merge_sort(vec, meio + 1, fim);
+        merge(vec, ini, fim, meio);
     }
-    int meio = (ini + fim) / 2;
-    merge_sort(vec, ini, meio);
-    merge_sort(vec, meio + 1, fim);
-
-    merge(vec, ini, fim);
+    return;
 }
 
 int main(void) {
@@ -33,6 +61,11 @@ int main(void) {
     clock_t inicio = clock();
     merge_sort(vec, 0, n-1);
     clock_t fim = clock();
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vec[i]);
+    }
+    printf("\n");
 
     printf("%lf\n", (double)(fim - inicio) / CLOCKS_PER_SEC);
 
